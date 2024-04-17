ba0547192cd8d5d0e787fedfe417edcb21b3d211
Joffrey Czarny <jczarny@medallia.com>
2024-04-17 13:58:42 +0800

    Fixing/Adding deps for Clamav v1.3.0

--- example/prepare.sh
+++ example/prepare.sh
@@ -2,18 +2,22 @@
 
 # make static lib for clamav deps
 apt-get update
-apt-get install -y python3-pip
+apt-get install -y python3-pip git
 apt-get install -y build-essential clang llvm
 apt-get install -y libstdc++6 libstdc++-6-dev
-apt-get install -y flex bison python3-dev pkg-config ninja-build
+## Adding more depencies especially rust as it's needed form Clamav
+apt-get install -y flex bison python3-dev pkg-config ninja-build rust-all gcc make python3-pytest valgrind check libbz2-dev libcurl4-openssl-dev libjson-c-dev libmilter-dev libncurses5-dev libpcre2-dev libssl-dev libxml2-dev zlib1g-dev
 python3 -m pip install mussels
 
-wget https://cmake.org/files/v3.21/cmake-3.21.5-linux-x86_64.tar.gz
-tar -zxvf cmake-3.21.5-linux-x86_64.tar.gz -C /usr/local/
-ln -s /usr/local/cmake-3.21.5-linux-x86_64/bin/cmake /usr/bin/cmake
-rm -rf cmake-3.21.5-linux-x86_64.tar.gz
+## Adding cmake via pip to have a recent verion
+# wget https://cmake.org/files/v3.21/cmake-3.21.5-linux-x86_64.tar.gz
+# tar -zxvf cmake-3.21.5-linux-x86_64.tar.gz -C /usr/local/
+# ln -s /usr/local/cmake-3.21.5-linux-x86_64/bin/cmake /usr/bin/cmake
+# rm -rf cmake-3.21.5-linux-x86_64.tar.gz
+python3 -m pip install cmake
 
-git clone --depth 1 https://github.com/ca110us/clamav-mussels-cookbook.git
+#git clone --depth 1 https://github.com/ca110us/clamav-mussels-cookbook.git
+git clone --depth 1 https://github.com/Cisco-Talos/clamav-mussels-cookbook
 cd clamav-mussels-cookbook
 rm -rf  mussels/* &> /dev/null
 mkdir mussels &> /dev/null
@@ -23,7 +27,8 @@ cd -
 # make get clamav source code
 git clone https://github.com/Cisco-Talos/clamav.git
 cd clamav
-git checkout clamav-0.104.0
+#git checkout clamav-0.104.0
+git checkout clamav-1.3.0
 cd -
 
 
@@ -73,8 +78,11 @@ cp clamav/build/libclamav/libclamav_static.a ./lib
 cp clamav/build/libclammspack/libclammspack_static.a ./lib
 cp clamav/build/libclamunrar/libclamunrar_static.a ./lib
 cp clamav/build/libclamunrar_iface/libclamunrar_iface_static.a ./lib
+## adding libclamav_rust
+cp clamav/build/x86_64-unknown-linux-gnu/release/libclamav_rust.a ./lib
 
-cp "$CLAMAV_DEPENDENCIES/lib/libbz2_static.a" ./lib
+#cp "$CLAMAV_DEPENDENCIES/lib/libbz2_static.a" ./lib
+cp "$CLAMAV_DEPENDENCIES/lib/libbz2.a" ./lib
 cp "$CLAMAV_DEPENDENCIES/lib/libjson-c.a" ./lib
 cp "$CLAMAV_DEPENDENCIES/lib/libcrypto.a" ./lib
 cp "$CLAMAV_DEPENDENCIES/lib/libssl.a" ./lib
